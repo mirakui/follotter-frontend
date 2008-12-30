9b6a3b6c19bcbf44c8fda97092eb6e6acac9f6b6
inaruta <inaruta@mirakuieee.(none)>
2008-12-30 13:26:22 +0900

    Changed @base_uri with ENV['BASE_URI']

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,7 +3,6 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  before_filter :set_base_uri_filter
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
@@ -13,8 +12,4 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
-
-  def set_base_uri_filter
-    @base_uri = request.protocol + request.host_with_port
-  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,9 @@
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
+ENV['BASE_URI'] = ENV['RAILS_ENV']=='production' ?
+  'http://mirakui.tsuyabu.in/follotter' :
+  'http://localhost:4000'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
